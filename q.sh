48b1ecb6d99fe53dd2798a05090c41458dd91652
girllee <hodoinfo@gmail.com>
2020-11-03 17:57:25 +0800

    test

--- q.sh
+++ q.sh
@@ -1,29 +1,15 @@
+# check if has folder ~/.dev
+tgt=~/.dex
+echo $tgt
+if [ ! -d $tgt ]; then
+    mkdir -p $tgt
+fi
 
 
-if [ ! -f ~/.bashrc ]; then
+if [ ! -f $tgt/.q ]; then
     echo "parsing .env ..."
-    wget https://raw.githubusercontent.com/AsinRay/q/master/env/.env -O ~/.env
+    wget https://raw.githubusercontent.com/AsinRay/q/master/.env/.q -O $tgt/.q
 fi
 
-
-if [ ! -f ~/.bashrc ]; then
-    echo "parsing .bashrc ..."
-    wget https://raw.githubusercontent.com/AsinRay/q/master/env/.bashrc -O ~/.bashrc
-fi
-
-echo "写入文件"
-cat>>~/.bashrc<<EOF
-if [ -f ~/.env ]; then
-    . ~/.env
-fi
-EOF
-
-
-
-if [ ! -f ~/.profile ]; then
-    echo "parsing .profile ..."
-    wget https://raw.githubusercontent.com/AsinRay/q/master/env/.profile -O ~/.profile
-    . ~/.profile
-fi
 
 
